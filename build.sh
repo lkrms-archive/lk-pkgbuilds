9600271b0f83cc6a84c7744f9a4ccb1913b1ae3c
Luke Arms <luke@linacreative.com>
2020-07-04 16:37:50 +1000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+for PKGBUILD in */PKGBUILD; do
+    # update pkgver before generating SRCINFO files
+    pushd "$(dirname "$PKGBUILD")" >/dev/null &&
+        makepkg --cleanbuild --nobuild &&
+        makepkg --printsrcinfo >".SRCINFO" || exit
+    popd >/dev/null
+done
+
+QUEUE_FILE="$(mktemp)"
+cat -- */.SRCINFO | aur graph | tsort | tac >"$QUEUE_FILE"
+aur build -d "${1:-lk-aur}" -a "$QUEUE_FILE"
