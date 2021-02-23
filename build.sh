7767cfe02f1ae8b013360127a180c75e7d5d2f09
Luke Arms <luke@arms.to>
2021-02-23 20:39:42 +1100

    Add --prepare option to build.sh

--- build.sh
+++ build.sh
@@ -3,23 +3,24 @@
 set -euo pipefail
 shopt -s nullglob
 
+PREPARE=
+[[ ! ${1:-} =~ ^(--)?prepare$ ]] || PREPARE=1
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+set -- */PKGBUILD
 if [ $# -eq 0 ]; then
-    cd "$(dirname "${BASH_SOURCE[0]}")"
-    set -- */PKGBUILD
-    if [ $# -eq 0 ]; then
-        echo "Nothing to build" >&2
-        exit 1
-    fi
+    echo "Nothing to build" >&2
+    exit 1
 fi
 
-for PKGBUILD in "$@"; do
-    # Update pkgver before generating SRCINFO files
-    (cd "$(dirname "$PKGBUILD")" &&
-        makepkg --syncdeps --noconfirm --nobuild &&
-        makepkg --printsrcinfo >.SRCINFO)
-done
-
-if type -P aur >/dev/null; then
+if [ -n "$PREPARE" ]; then
+    for PKGBUILD in "$@"; do
+        # Update pkgver before generating SRCINFO files
+        (cd "$(dirname "$PKGBUILD")" &&
+            makepkg --syncdeps --noconfirm --nobuild &&
+            makepkg --printsrcinfo >.SRCINFO)
+    done
+elif type -P aur >/dev/null; then
     QUEUE=$(mktemp)
     aur graph "${@/PKGBUILD/.SRCINFO}" | tsort | tac >"$QUEUE"
     aur build --database lk --noconfirm --force \
