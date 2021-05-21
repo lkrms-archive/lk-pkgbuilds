64b60c7ab98f787c1571d66a2864220e788e820c
Luke Arms <luke@arms.to>
2021-05-21 14:05:54 +1000

    Update build script
    Also:
    - Add remmina and freerdp
    - Remove geekbench4
    - Update xfce4-settings

--- build.sh
+++ build.sh
@@ -5,37 +5,71 @@ shopt -s nullglob
 
 PREPARE=
 FORCE=
+ALL=
 while [ $# -gt 0 ]; do
-    case "${1#--}" in
-    prepare)
+    case "$1" in
+    --prepare)
         PREPARE=1
         ;;
-    force)
+    --force)
         FORCE=1
+        ;;
+    --all)
+        ALL=1
+        ;;
+    *)
+        break
         ;;
     esac
     shift
 done
 
+[[ "${ALL:-0}$#" =~ ^(0[1-9][0-9]*|10)$ ]] || {
+    echo "Usage: ${0##*/} [--prepare] [--force] --all | PACKAGE..." >&2
+    exit 1
+}
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
-set -- */PKGBUILD
+
+if [ $# -eq 0 ]; then
+    set -- */PKGBUILD
+else
+    PKGBUILDS=()
+    for PACKAGE in "$@"; do
+        [[ $PACKAGE =~ ^([^/]+)(/|/PKGBUILD)?$ ]] &&
+            PKGBUILD=${BASH_REMATCH[1]}/PKGBUILD &&
+            [ -f "$PKGBUILD" ] || {
+            echo "Invalid package: $PACKAGE" >&2
+            exit 1
+        }
+        PKGBUILDS[${#PKGBUILDS[@]}]=$PKGBUILD
+    done
+    set -- "${PKGBUILDS[@]}"
+fi
+
 if [ $# -eq 0 ]; then
     echo "Nothing to build" >&2
     exit 1
 fi
 
+{
+    echo "Building:"
+    printf -- '- %s\n' "${@%\/PKGBUILD}"
+} >&2
+
 if [ -n "$PREPARE" ]; then
     for PKGBUILD in "$@"; do
         # Update pkgver before generating SRCINFO files
         (cd "$(dirname "$PKGBUILD")" &&
-            makepkg --syncdeps --noconfirm --nobuild &&
+            makepkg --nodeps --noconfirm --nobuild &&
             makepkg --printsrcinfo >.SRCINFO)
     done
 elif type -P aur >/dev/null; then
     QUEUE=$(mktemp)
     aur graph "${@/PKGBUILD/.SRCINFO}" | tsort | tac >"$QUEUE"
-    aur build --database lk --noconfirm ${FORCE:+--force} \
+    aur build --database lk --noconfirm ${FORCE:+--force} --remove \
         --chroot --makepkg-conf=/etc/makepkg.conf \
+        ${GPGKEY+--sign} \
         --arg-file "$QUEUE"
     rm -f "$QUEUE"
 else
