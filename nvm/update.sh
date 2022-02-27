2ad0f1294064a43d3a32e9e999557b089b1c5c79
Luke Arms <luke@arms.to>
2022-02-27 23:49:05 +1100

    Update httptoolkit

--- nvm/update.sh
+++ nvm/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if (( $# != 1 )); then
+if (($# != 1)); then
     echo "Usage: $0 VERSION"
     echo "Update pkgver to VERSION and then build and commit."
     echo "You just need to test the package, review the commit and push."
@@ -13,7 +13,7 @@ set -x
 
 sed -i "s/^pkgver=.*\$/pkgver=${VERSION}/" PKGBUILD
 updpkgsums
-makepkg --printsrcinfo > .SRCINFO
+makepkg --printsrcinfo >.SRCINFO
 git add .
 git commit -m "Upgrade to v${VERSION}"
 makepkg -i
